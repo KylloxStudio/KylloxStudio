e7759bbf2abb760cced029eb0497361b4a2619be
KylloxStudio <Kyllox4804@gmail.com>
2021-03-11 06:20:41 +0900

    Update

---
+++ scripts/scripts.js
@@ -0,0 +1,29 @@
+/* Template: Aria - Business HTML Landing Page Template
+   Author: Inovatik
+   Created: Jul 2019
+   Description: Custom JS file
+*/
+
+
+(function($) {
+    "use strict"; 
+	
+	/* Navbar Scripts */
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 750, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+
+    // closes the responsive menu on menu item click
+    $(".navbar-nav li a").on("click", function(event) {
+    if (!$(this).parent().hasClass('dropdown'))
+        $(".navbar-collapse").collapse('hide');
+    });
+
+})(jQuery);
